24a4024b0315087ad5b8b1d0b742c8041cba495f
ywchiang0819121 <ywchiang0819121@gmail.com>
2019-11-03 18:29:48 +0800

    123

--- Homework3/Hw3.py
+++ Homework3/Hw3.py
@@ -30,12 +30,84 @@ def EM_trainging(row, col, word_doc, word_topic, topic_doc, isFoldin = False):
         thresh = FOLDIN_THRESH
     else:
         thresh = THRESH
-    E_rsult = np.zeros((TOPICNUMBER, len(word_doc)), dtype=np.float)
+    E_result = np.zeros((TOPICNUMBER, len(word_doc)), dtype=np.float)
 
     for i in range(MAX_ITER):
+        E_result = E_step(row, col, word_doc, word_topic, topic_doc, E_result)
+        if isFoldin:
+            topic_doc, _ = M_step(row, col, word_doc, word_topic, topic_doc, E_result, isFoldin)
+        else:
+            topic_doc, word_topic = M_step(row, col, word_doc, word_topic, topic_doc, E_result, isFoldin)
+        L = get_L(row, col, word_doc, word_topic, topic_doc)
+        print('{}, L = {}'.format(i, L))
+        if L_last == 0:
+            L_last = L
+        elif (abs(L - L_last)  / abs(L_last)) < thresh:
+            print('early stopping...')
+            return topic_doc, word_topic
+        L_last = L
+    return topic_doc, word_topic
+
         
 @jit(nopython=True)
+def E_step(row, col, word_doc, word_topic, topic_doc, E_result):
+    for i in range(len(word_doc)):
+        dj, wi = row[i], col[i]
+        joint_prob = np.zeros(TOPICNUMBER)
+        joint_prob_sum = 0
 
+        for j in range(TOPICNUMBER):
+            joint_prob[j] = word_topic[wi][j] * topic_doc[j][dj]
+            joint_prob_sum += joint_prob[j]
+        for j in range(TOPICNUMBER):
+            if joint_prob_sum = 0:
+                E_result[j][i] = 0
+            else:
+                E_result[j][i] = joint_prob[j] / joint_prob_sum
+    return E_result
+
+@jit(nopython=True)
+def M_step(row, col, word_doc, word_topic, topic_doc, E_result, isFoldin):
+    wordNum = len(word_topic)
+    docNum  = len(topic_doc[0])
+    word_topic_temp = np.zeros(word_topic.shape)
+    topic_doc_temp  = np.zeros(topic_doc.shape)
+    word_topic_sum  = np.zeros(TOPICNUMBER)
+    topic_doc_sum   = np.zeros(docNum)
+
+    for i in range(len(word_doc)):
+        dj, wi = row[i], col[i]
+        for j in range(TOPICNUMBER):
+            result = word_doc[i] * E_result[j][i]
+            topic_doc_sum[dj] += result
+            topic_doc_temp[k][dj] += result
+            if isFoldin == False:
+                word_topic_sum[j] += result
+                word_topic_temp[wi][j] += result
+    for i in range(docNum):
+        for j in range(TOPICNUMBER):
+            if topic_doc_sum[i] != 0:
+                topic_doc_temp[j][i] /= topic_doc_sum[j]
+    if isFoldin == False:
+        for i in range(TOPICNUMBER):
+            for j in range(wordNum):
+                if word_topic_sum[i] != 0:
+                    word_topic_temp[j][i] /= word_topic_sum[i]
+    return topic_doc_temp, word_topic_temp
+
+@jit(nopython=True)
+def get_L(row, col, word_doc, word_topic, topic_doc):
+    L = 0
+    for i in range(len(word_doc)):
+        di, wi = row[i], col[i]
+        result = 0
+        for j in range(TOPICNUMBER):
+            result += float(word_topic[wi][j]) * float(topic_doc[j][di])
+        result *= float(1 / len(topic_doc[0]))
+        if result != 0:
+            result = math.log(result)
+        L += word_doc[i] * result
+    return L
 
 if __name__ == '__main__':
     print('reading file...')
